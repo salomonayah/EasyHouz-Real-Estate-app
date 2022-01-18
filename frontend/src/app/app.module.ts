17774c449cf15d9bffc6887ab8b1fd33fde39b2e
Salomon AYAH <salomonayah@gmail.com>
2022-01-18 14:13:56 +0100

    Frontend: fonts installed - Layout component done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './layout-component/topbar/topbar.component';
+import { FooterComponent } from './layout-component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
