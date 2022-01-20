520a96a1668b00d6bb02945b6d64a1172710308e
Salomon AYAH <salomonayah@gmail.com>
2022-01-20 01:26:00 +0100

    Routing and app modules added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from './auth/auth-guard/auth-guard';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((mod) => mod.AuthModule),
+  },
+  {
+    path: 'main',
+    canActivateChild: [AuthGuard],
+    loadChildren: () => import('./core/core.module').then((mod) => mod.CoreModule),
+  },
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then((mod) => mod.HomeModule),
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
